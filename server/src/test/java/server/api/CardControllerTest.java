9e3fe7e0fc4ed2908d2363ed2a868c3fdd13cee3
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-03-01 18:20:14 +0100

    added tests for the cardController

--- server/src/test/java/server/api/CardControllerTest.java
+++ server/src/test/java/server/api/CardControllerTest.java
@@ -1,4 +1,44 @@
 package server.api;
 
+import commons.Card;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.ResponseEntity.ok;
+
 public class CardControllerTest {
+    private TestCardRepository testCardRepository;
+    private CardController cardController;
+
+    @BeforeEach
+    public void setup() {
+        this.testCardRepository = new TestCardRepository();
+        this.cardController = new CardController(testCardRepository);
+    }
+
+    @Test
+    public void canAddCard() {
+        Card card = new Card("new card 1");
+        assertEquals(HttpStatus.OK, cardController.add(card).getStatusCode());
+    }
+
+    @Test
+    public void newCardIsActuallyAdded() {
+        Card card = new Card("new card 1");
+        cardController.add(card);
+        assertTrue(testCardRepository.existsById(card.getId()));
+    }
+
+    @Test
+    public void testGetById() {
+        Card card1 = new Card("new card 1");
+        Card card2 = new Card("new card 2");
+        cardController.add(card1);
+        cardController.add(card2);
+        assertEquals(card1, cardController.getById(card1.getId()).getBody());
+    }
 }
