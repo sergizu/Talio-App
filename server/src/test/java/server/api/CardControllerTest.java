96cfa6a153ce2fced53b21aa52e49f4a3f12f7ae
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-03-01 18:31:58 +0100

    removed unused imports

--- server/src/test/java/server/api/CardControllerTest.java
+++ server/src/test/java/server/api/CardControllerTest.java
@@ -4,11 +4,9 @@ import commons.Card;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.http.ResponseEntity.ok;
 
 public class CardControllerTest {
     private TestCardRepository testCardRepository;
