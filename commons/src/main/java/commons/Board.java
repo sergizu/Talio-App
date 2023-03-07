23105d24f5555479dc0d26d5759cb9c2b9adeb39
Emils Dzintars <E.Dzintars@student.tudelft.nl>
2023-03-07 17:20:18 +0100

    created new Board class, controller, repository with tests

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
