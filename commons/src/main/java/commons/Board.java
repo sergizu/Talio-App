73dfc3886f15385871abb6db0584455b89ba2f89
Sergiu Nicolae Stancu <s.n.stancu@student.tudelft.nl>
2023-03-07 16:44:04 +0100

    I have changed the equals method to be shorter

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,5 +1,6 @@
 package commons;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,11 +23,8 @@ public class Board {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Board b = (Board) o;
-        return Objects.equals(id, b.id) && Objects.equals(lists, b.lists);
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
     }
 
     @Override
