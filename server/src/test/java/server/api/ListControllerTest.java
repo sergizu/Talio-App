6e6d2d7a80525ccab3c21b671abdbe40121db84d
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-03-07 13:16:33 +0100

    changed typo in TestListRepostry to TestListRepository

--- server/src/test/java/server/api/ListControllerTest.java
+++ server/src/test/java/server/api/ListControllerTest.java
@@ -4,18 +4,13 @@ import org.junit.jupiter.api.Test;
 import server.database.ListRepository;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-
-
-
-
-    class ListControllerTest {
+class ListControllerTest {
         private ListRepository listRepository;
 
         @BeforeEach
         public void setup() {
-            listRepository = new TestListRepositry();
+            listRepository = new TestListRepository();
         }
 
         @Test
--- server/src/test/java/server/api/TestListRepository.java
+++ server/src/test/java/server/api/TestListRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class TestListRepositry implements ListRepository {
+public class TestListRepository implements ListRepository {
 
 
     @Override
