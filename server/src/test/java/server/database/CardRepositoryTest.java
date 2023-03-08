bb84d19c1eda24e4ef55ec32f47c37ade0e424b1
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-03-08 13:48:39 +0100

    added tests for cardRepository

--- server/src/test/java/server/database/CardRepositoryTest.java
+++ server/src/test/java/server/database/CardRepositoryTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
-class CardRepositoryTest {
-  
+package server.database;
+
+import commons.Card;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class CardRepositoryTest {
+
+    @Autowired
+    CardRepository cardRepository;
+
+    @Test
+    public void addCard() {
+        Card card = new Card("Card 1");
+        cardRepository.save(card);
+        assertTrue(cardRepository.existsById(card.id));
+    }
 }
+
