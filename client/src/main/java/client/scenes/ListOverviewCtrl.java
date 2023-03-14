3c729afb7181d4a6e92469bba126f4efae3788c4
Aadesh Pranay Ramai <a.p.ramai@student.tudelft.nl>
2023-03-14 15:28:14 +0100

    Added drag and drop feature, does not change anything in the database but should be easy using an updateList method,

--- client/src/main/java/client/scenes/ListOverviewCtrl.java
+++ client/src/main/java/client/scenes/ListOverviewCtrl.java
@@ -13,7 +13,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,8 +30,11 @@ public class ListOverviewCtrl implements Initializable {
     private Board board;
     private ObservableList<ObservableList<Card>> dataLists;
 
+    private static final DataFormat serialization = new DataFormat("application/x-java-serialized-object");
+
     @FXML private Label label1;
     @FXML private TableView<Card> tableView;
+
     @FXML private TableColumn<Card, String> cardColumn;
 
     @Override
@@ -38,7 +46,7 @@ public class ListOverviewCtrl implements Initializable {
                 //adding the card to the most left list(TO-DO)
         });
         cardExpansion();
-
+        dragAndDrop();
     }
 
     //boardID is not yet used
@@ -85,6 +93,63 @@ public class ListOverviewCtrl implements Initializable {
     public void editListName() {
         TDList list = board.lists.get(0);
         mainCtrl.showEditList(list);
+    }
+
+    public void dragAndDrop() {
+        tableView.setRowFactory(tv -> {
+            TableRow<Card> row = new TableRow<>();
+            row.setOnDragDetected(e -> {
+                //Method gets called whenever a mouse drags a row
+                if (!row.isEmpty()) {
+                    int i = row.getIndex();
+                    Dragboard db = row.startDragAndDrop(TransferMode.MOVE);
+                    db.setDragView(row.snapshot(null, null));
+                    //shows a snapshot of the row when moving it
+                    ClipboardContent cc = new ClipboardContent();
+                    cc.put(serialization, i);
+                    db.setContent(cc);
+                    //makes it so that you can find the row index in the dragboard
+                    e.consume(); //Marks the end of the event
+                }
+            });
+
+            row.setOnDragOver(e -> {
+                Dragboard db = e.getDragboard();
+                if (db.hasContent(serialization)) {
+                    //Checks whether the dataformat has any information, which it should have as
+                    //it has been associated with the row Index in the setOnRowDetected method
+                        e.acceptTransferModes(TransferMode.MOVE);
+                        //accepts the drag event
+                        e.consume();
+                }
+            });
+
+            row.setOnDragDropped(e -> {
+                Dragboard db = e.getDragboard();
+                if (db.hasContent(serialization)) {
+                    int draggedIndex = (int) db.getContent(serialization);
+                    Card card = tableView.getItems().remove(draggedIndex);
+                    //gets the rowIndex and removes the Card at it's position
+                    int dropIndex;
+                    if (row.isEmpty()) {
+                        dropIndex = tableView.getItems().size() ;
+                    } else {
+                        dropIndex = row.getIndex();
+                    }
+                    tableView.getItems().add(dropIndex, card);
+                    //If the row we drop the card on is empty, the card will be appended to
+                    //the end of the tableview, otherwise it's dropped at the row index and
+                    //added to the tableview items list
+
+                    e.setDropCompleted(true); //marks the end of the drag event
+                    tableView.getSelectionModel().select(dropIndex);
+                    //Selects the dropped card, otherwise the first card in the tableview
+                    //would be selected, which would not make sense
+                    e.consume();
+                }
+            });
+            return row ;
+        });
     }
 }
 
