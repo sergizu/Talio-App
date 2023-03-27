a0adce99253812c052bc545997f273cfee871aeb
Emils Dzintars <E.Dzintars@student.tudelft.nl>
2023-03-27 17:38:54 +0200

    AddCardService modified without passing FXML objects. It should now be possible to test it.

--- client/src/main/java/client/services/AddCardService.java
+++ client/src/main/java/client/services/AddCardService.java
@@ -4,8 +4,6 @@ import client.utils.ServerUtils;
 import commons.Card;
 import jakarta.ws.rs.WebApplicationException;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
@@ -24,13 +22,13 @@ public class AddCardService {
     }
 
     public void ok( ServerUtils server, long listId, Card card) {
-            try {
-                server.addCardToList(listId, card);
-            } catch (WebApplicationException e) {
-                var alert = new Alert(Alert.AlertType.ERROR);
-                alert.initModality(Modality.APPLICATION_MODAL);
-                alert.setContentText(e.getMessage());
-                alert.showAndWait();
-            }
+        try {
+            server.addCardToList(listId, card);
+        } catch (WebApplicationException e) {
+            var alert = new Alert(Alert.AlertType.ERROR);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
     }
 }
