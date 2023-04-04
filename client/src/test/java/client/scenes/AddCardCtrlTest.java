8c151a7f4805544397dd676f0836e93004cd1dc4
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-04-04 22:54:14 +0200

    Wrote some basic tests for the renewed layout

---
+++ client/src/test/java/client/scenes/AddCardCtrlTest.java
@@ -0,0 +1,87 @@
+package client.scenes;
+
+import client.services.AddCardService;
+import client.utils.ServerUtils;
+import commons.Card;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AddCardCtrlTest{
+    @Mock
+    ServerUtils serverUtils;
+    @Mock
+    MainCtrl mainCtrl;
+    @Mock
+    AddCardService addCardService;
+
+    static AddCardCtrl addCardCtrl;
+
+    @BeforeEach
+    public void setUp() {
+        addCardCtrl = new AddCardCtrl(serverUtils, mainCtrl, addCardService);
+    }
+    
+    @Test
+    public void testConstructor() {
+        assertNotNull(new AddCardCtrl(serverUtils, mainCtrl, addCardService));
+    }
+
+    @Test
+    public void testSetListBoardId() {
+        addCardCtrl.setListBoardId(1, 2);
+        assertEquals(1, addCardCtrl.getListId());
+        assertEquals(2, addCardCtrl.getBoardId());
+    }
+
+    @Test
+    public void testOk() {
+        given(addCardService.getCardName()).willReturn("");
+        addCardCtrl.ok();
+        verify(addCardService).setMyLabelText("Card name cannot be empty!");
+        verify(addCardService, never()).clearFields();
+    }
+
+    @Test
+    public void testOk2() {
+        given(addCardService.getCardName()).willReturn("Card 1");
+        addCardCtrl.setListBoardId(1, 2);
+        addCardCtrl.ok();
+
+        verify(addCardService, never()).setMyLabelText("Card name cannot be empty!");
+        verify(addCardService).clearFields();
+        verify(serverUtils).addCardToList(1, new Card("Card 1"));
+    }
+
+    @Test
+    public void testCancel() {
+        addCardCtrl.cancel();
+        verify(addCardService).clearFields();
+    }
+
+
+    @Test
+    public void testKeyPressedEnter() {
+        given(addCardService.getCardName()).willReturn("");
+        addCardCtrl.keyPressed(new KeyEvent(KeyEvent.KEY_PRESSED, "", "", KeyCode.ENTER, false, false, false, false));
+        verify(addCardService).setMyLabelText("Card name cannot be empty!");
+    }
+
+    @Test
+    public void testKeyPressedEscape() {
+        addCardCtrl.keyPressed(new KeyEvent(KeyEvent.KEY_PRESSED, "", "", KeyCode.ESCAPE, false, false, false, false));
+        verify(addCardService).clearFields();
+        verify(mainCtrl).showOverview(anyLong());
+    }
+}
