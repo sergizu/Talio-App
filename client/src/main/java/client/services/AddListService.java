bc406756d357a4f624231ddf498e958027f136b7
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-04-05 21:50:54 +0200

    Added @ImplementedBy annotation to improve readability

--- client/src/main/java/client/services/AddListService.java
+++ client/src/main/java/client/services/AddListService.java
@@ -1,5 +1,8 @@
 package client.services;
 
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(AddListServiceImpl.class)
 public interface AddListService {
     void setEmptyNameText(String text);
     String getListTitle();
