52cff4af366a87763acdc54099c6ee3c66311824
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-04-05 21:52:34 +0200

    Created Interface to enable proxying

--- client/src/main/java/client/scenes/AddSubTaskCtrl.java
+++ client/src/main/java/client/scenes/AddSubTaskCtrl.java
@@ -1,50 +1,12 @@
 package client.scenes;
 
-import client.services.AddSubTaskService;
-import client.utils.ServerUtils;
-import com.google.inject.Inject;
+import com.google.inject.ImplementedBy;
 import commons.Card;
-import commons.Subtask;
 
-public class AddSubTaskCtrl {
-
-    private final ServerUtils server;
-
-    private final MainCtrl mainCtrl;
-    private final AddSubTaskService addSubTaskService;
-
-    private Card card;
-
-    @Inject
-    public AddSubTaskCtrl(ServerUtils server, MainCtrl mainCtrl,
-              AddSubTaskService addSubTaskService) {
-        this.server = server;
-        this.mainCtrl = mainCtrl;
-        this.addSubTaskService = addSubTaskService;
-    }
-
-    public void init(Card card) {
-        this.card = card;
-    }
-
-    public void create() {
-        if(addSubTaskService.getSubtaskName().isEmpty()) {
-            addSubTaskService.setMyLabelText("Name cannot be empty!");
-        } else {
-            String name = addSubTaskService.getSubtaskName();
-            addSubTaskService.setSubtaskName("");
-            Subtask subtask = new Subtask(name);
-            card.addSubTask(subtask);
-            server.updateNestedList(card.id, card.nestedList);
-            mainCtrl.showEdit(card);
-        }
-    }
-    public void cancel() {
-        addSubTaskService.setMyLabelText("");
-        mainCtrl.showOverview(card.list.board.id);
-    }
-
-    public Card getCard() {
-        return card;
-    }
+@ImplementedBy(AddSubTaskCtrlImpl.class)
+public interface AddSubTaskCtrl {
+    void init(Card card);
+    void create();
+    void cancel();
+    Card getCard();
 }
