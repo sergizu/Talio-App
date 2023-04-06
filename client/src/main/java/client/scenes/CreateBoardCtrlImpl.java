5de8573d2b924c23903d0b5080a6d2adecefa69c
Konstantin Teplykh <K.Teplykh@student.tudelft.nl>
2023-04-06 13:44:55 +0200

    Created the service files for the CreateBoard controller

---
+++ client/src/main/java/client/scenes/CreateBoardCtrlImpl.java
@@ -0,0 +1,89 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import commons.AppClient;
+import commons.Board;
+import commons.TDList;
+import jakarta.ws.rs.WebApplicationException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
+
+import java.util.ArrayList;
+
+public class CreateBoardCtrl {
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+    private Object parent;
+
+    @FXML
+    TextField boardTitle;
+
+    @Inject
+    public CreateBoardCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    public Board getBoardWithTitle() {
+        Board board = new Board(boardTitle.getText());
+        addDefaultLists(board);
+        return board;
+    }
+
+    public void addDefaultLists(Board board) {
+        board.addList(new TDList("TO DO"));
+        board.addList(new TDList("DOING"));
+        board.addList(new TDList("DONE"));
+    }
+
+    public void createBoard() {
+        Board board = getBoardWithTitle();
+        boardTitle.setText("");
+        try {
+            board = server.addBoard(board);
+            if (parent == JoinedBoardsCtrl.class)
+                addBoardToClient(board);
+        } catch (WebApplicationException e) {
+            var alert = new Alert(Alert.AlertType.ERROR);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+            return;
+        }
+        mainCtrl.showOverview(board.getId());
+    }
+
+    public void addBoardToClient(Board board) {
+        AppClient client = mainCtrl.getClient();
+        ArrayList<Board> boards = client.boards.get(ServerUtils.getServer());
+        if (boards == null)
+            boards = new ArrayList<>();
+        boards.add(board);
+        client.boards.put(ServerUtils.getServer(), boards);
+    }
+
+    public void escape() {
+        if (parent == JoinedBoardsCtrl.class)
+            mainCtrl.showJoinedBoards();
+        else if (parent == BoardOverviewCtrl.class) {
+            mainCtrl.showBoardOverview();
+        }
+    }
+
+    public void setParent(Object parent) {
+        this.parent = parent;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getCode() == KeyCode.ENTER) {
+            createBoard();
+        } else if (e.getCode() == KeyCode.ESCAPE) {
+            escape();
+        }
+    }
+}
---
+++ client/src/main/java/client/services/CreateBoardService.java
@@ -0,0 +1,12 @@
+package client.services;
+
+import com.google.inject.ImplementedBy;
+import javafx.scene.input.KeyEvent;
+
+@ImplementedBy(CreateBoardServiceImp.class)
+public interface CreateBoardService {
+    void setBoardName(String s);
+    String getBoardName();
+    void createBoard();
+    void escape();
+}
