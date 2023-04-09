326cc60e676b2f3362681f81943e3fd2851bd7bc
Aadesh Pranay Ramai <a.p.ramai@student.tudelft.nl>
2023-04-09 21:06:47 +0200

    Created an interface for the editcard service which contains the outline of all the methods that can be called from the UI side of the app

---
+++ client/src/main/java/client/services/interfaces/EditCardService.java
@@ -0,0 +1,27 @@
+package client.services.interfaces;
+
+import client.helperClass.SubtaskWrapper;
+import client.services.implementations.EditCardServiceImpl;
+import com.google.inject.ImplementedBy;
+import javafx.scene.control.TableColumn;
+import java.util.List;
+@ImplementedBy(EditCardServiceImpl.class)
+public interface EditCardService {
+
+    void setCardName(String title);
+
+    void setDescription(String description);
+
+    String getCardName();
+
+    String getDescription();
+
+    void initTableView(List<SubtaskWrapper> subtaskWrappers);
+
+    void setEmptyName(String s);
+
+    void changeSubtask(TableColumn.CellEditEvent<SubtaskWrapper, String> edit);
+
+    void dragAndDrop();
+
+}
