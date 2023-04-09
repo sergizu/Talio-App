2dcf3ec25373bb7bd17b12758711891dc464206b
Emils Dzintars <E.Dzintars@student.tudelft.nl>
2023-04-10 00:15:52 +0200

    new SelectServerCtrlImpl

---
+++ client/src/main/java/client/scenes/implementations/SelectServerCtrlImpl.java
@@ -0,0 +1,92 @@
+package client.scenes.implementations;
+
+
+import client.scenes.*;
+import client.scenes.SelectServerCtrl;
+import client.scenes.interfaces.EditCardCtrl;
+import client.scenes.interfaces.JoinedBoardsCtrl;
+import client.services.interfaces.SelectServerService;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.concurrent.Executors;
+
+@Singleton
+public class SelectServerCtrlImpl implements SelectServerCtrl {
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+    private final ListOverviewCtrl listOverviewCtrl;
+    private JoinedBoardsCtrl joinedBoardsCtrl;
+
+    private final BoardOverviewCtrl boardOverviewCtrl;
+
+    private final EditCardCtrl editCardCtrl;
+    private final SelectServerService selectServerService;
+
+    @Inject
+    public SelectServerCtrlImpl(ServerUtils server, MainCtrl mainCtrl,
+                                ListOverviewCtrl listOverviewCtrl,
+                                JoinedBoardsCtrl joinedBoardsCtrl,
+                                BoardOverviewCtrl boardOverviewCtrl,
+                                EditCardCtrl editCardCtrl, SelectServerService selectServerService) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+        this.listOverviewCtrl = listOverviewCtrl;
+        this.boardOverviewCtrl = boardOverviewCtrl;
+        this.editCardCtrl = editCardCtrl;
+        this.joinedBoardsCtrl = joinedBoardsCtrl;
+
+        this.selectServerService = selectServerService;
+    }
+
+    public boolean checkPass() {
+        String password = selectServerService.getAdminPassText();
+        return password.equals("1010");
+    }
+
+    public void ok() {
+        String s = selectServerService.getServerNameText();
+        if (s.isEmpty()) {
+            selectServerService.setMyLabel("Can not be empty!");
+            return;
+        }
+        selectServerService.setMyLabel("");
+        server.changeServer(s);
+        if(server.serverRunning()){
+            startSession();
+            if (checkPass()) {
+                mainCtrl.setAdmin(true);
+                mainCtrl.showBoardOverview();
+                selectServerService.setAdminPassText("");
+                selectServerService.setBoxVisible(false);
+            } else {
+                mainCtrl.showJoinedBoards();
+                mainCtrl.setAdmin(false);
+            }
+        }
+        else{
+            selectServerService.setMyLabel("Couldn't find the server!");
+        }
+    }
+
+    public void adminLogIn() {
+        selectServerService.setBoxVisible(true);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getCode() == KeyCode.ENTER) {
+            ok();
+        }
+    }
+    public void startSession(){
+        server.setExecutorService(Executors.newCachedThreadPool());
+        server.initSession();
+        boardOverviewCtrl.registerForMessages();
+        joinedBoardsCtrl.registerForMessages();
+        listOverviewCtrl.init();
+        editCardCtrl.registerForUpdates();
+    }
+}
