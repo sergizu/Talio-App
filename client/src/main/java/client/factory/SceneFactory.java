3e2f5392712935e76a6253e4bd2f28598cba34ac
Danny Bunschoten <d.m.bunschoten@student.tudelft.nl>
2023-04-10 17:04:56 +0200

    Methods now use sceneCreator instead of new Scene()

--- client/src/main/java/client/factory/SceneFactory.java
+++ client/src/main/java/client/factory/SceneFactory.java
@@ -12,12 +12,15 @@ public class SceneFactory {
     private final Injector injector;
     private final MyFXML fxml;
 
-    private final int WIDTH = 1080;
-    private final int HEIGHT = 720;
+    private final SceneCreator sceneCreator;
 
-    public SceneFactory(Injector injector, MyFXML fxml) {
+    private final int width = 1080;
+    private final int heigth = 720;
+
+    public SceneFactory(Injector injector, MyFXML fxml, SceneCreator sceneCreator) {
         this.injector = injector;
         this.fxml = fxml;
+        this.sceneCreator = sceneCreator;
     }
 
 
@@ -28,60 +31,66 @@ public class SceneFactory {
     public Scene createListOverviewScene(){
         Parent parent = fxml.load(ListOverviewCtrl.class,
                 "client", "scenes", "ListOverview.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createEditCardScene(){
-        Parent parent = fxml.load(EditCardCtrl.class, "client", "scenes", "EditCard.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(EditCardCtrl.class, "client",
+                "scenes", "EditCard.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createSelectServerScene(){
-        Parent parent = fxml.load(SelectServerCtrl.class,"client","scenes", "SelectServer.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(SelectServerCtrl.class,"client","scenes",
+                "SelectServer.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createBoardOverviewScene(){
         Parent parent = fxml.load(BoardOverviewCtrl.class,
                 "client", "scenes", "BoardOverview.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createJoinedBoardsScene(){
         Parent parent = fxml.load(JoinedBoardsService.class,
                 "client", "scenes", "JoinedBoards.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createNewBoardScene(){
         Parent parent =  fxml.load(CreateBoardService.class,
                 "client", "scenes", "CreateBoard.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createAddSubtaskScene(){
-        Parent parent = fxml.load(AddSubTaskService.class, "client", "scenes", "AddSubtask.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(AddSubTaskService.class, "client",
+                "scenes", "AddSubtask.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createAddCardScene(){
-        Parent parent = fxml.load(AddCardService.class, "client", "scenes", "AddCard.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(AddCardService.class, "client",
+                "scenes", "AddCard.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createAddListScene(){
-        Parent parent = fxml.load(AddListService.class, "client", "scenes", "AddList.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(AddListService.class, "client",
+                "scenes", "AddList.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createEditListScene(){
-        Parent parent = fxml.load(EditListCtrl.class, "client", "scenes", "RenameList.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        Parent parent = fxml.load(EditListCtrl.class, "client",
+                "scenes", "RenameList.fxml").getValue();
+        return sceneCreator.createScene(parent);
     }
 
     public Scene createBoardOptionsScene() {
         Parent parent =  fxml.load(BoardOptionsCtrlImpl.class, "client",
                 "scenes", "BoardOptions.fxml").getValue();
-        return new Scene(parent, WIDTH, HEIGHT);
+        return sceneCreator.createScene(parent);
     }
 }
