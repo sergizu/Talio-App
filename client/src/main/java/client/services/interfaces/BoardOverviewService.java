2b05a37373080894cabc8baba4ad0be9f44725b6
Sergiu Nicolae Stancu <s.n.stancu@student.tudelft.nl>
2023-04-10 16:43:49 +0200

    restructured Boards Overview controller

--- client/src/main/java/client/services/interfaces/BoardOverviewService.java
+++ client/src/main/java/client/services/interfaces/BoardOverviewService.java
@@ -1,4 +1,19 @@
 package client.services.interfaces;
 
-public interface BoardOverviewService {
+import client.services.implementations.BoardOptionsServiceImpl;
+import com.google.inject.ImplementedBy;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+
+@ImplementedBy(BoardOptionsServiceImpl.class)
+public interface BoardOverviewService extends Initializable {
+
+    void createBoard();
+    void joinPressed(KeyEvent event);
+    void disconnectPressed();
+    void joinByKey();
+    void showAllBoards();
+    String getJoinByKeyText();
+    void clearJoinByKey();
+    void setJoinByKeyPrompt(String text);
 }
