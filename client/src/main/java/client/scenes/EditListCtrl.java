05ea1e6422f3eba1025cba97d5c5b46b6782d475
Aadesh Pranay Ramai <a.p.ramai@student.tudelft.nl>
2023-04-11 15:20:15 +0200

    removed unnecessary import

--- client/src/main/java/client/scenes/EditListCtrl.java
+++ client/src/main/java/client/scenes/EditListCtrl.java
@@ -3,7 +3,6 @@ package client.scenes;
 import client.scenes.interfaces.MainCtrl;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import commons.Card;
 import commons.TDList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
